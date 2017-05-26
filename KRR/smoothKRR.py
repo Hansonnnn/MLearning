1f2ba57c0f2c78cec3cd84f93ea80570145625b4
smithhan <1139562344@qq.com>
2017-05-26 23:13:32 +0800

    readme

--- KRR/smoothKRR.py
+++ KRR/smoothKRR.py
@@ -6,7 +6,6 @@ import dateutil.parser as dparser
 from pylab import *
 import mlpy
 
-#平滑黄金价格时间序列
 def smooth(x,window_len):
 
         s=np.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
@@ -17,32 +16,34 @@ def smooth(x,window_len):
 
 
 
-        y = np.genfromtxt("/Users/hanzhao/PycharmProjects/MLstudy/file/Gold.csv",
+y = np.genfromtxt("/Users/hanzhao/PycharmProjects/MLstudy/file/Gold.csv",
                      skip_header=1,
                      dtype=None,
                      delimiter=',',
                      usecols=(1))
 
 
-        targetValues = smooth(y, len(y))
+targetValues = smooth(y, len(y))
 
 
-        np.random.seed(10)
+np.random.seed(10)
 
-        trainingPoints = np.arange(125).reshape(-1, 1)
-        testPoints = np.arange(126).reshape(-1, 1)
+trainingPoints = np.arange(125).reshape(-1, 1)
+testPoints = np.arange(126).reshape(-1, 1)
 
-        kg=KernelGaussian()
-        knl = kg.kernel_gaussian(trainingPoints, trainingPoints)
-        knlTest = kg.kernel_gaussian(testPoints, trainingPoints, sigma=1)
+kg = KernelGaussian()
+knl = kg.kernel(trainingPoints, trainingPoints)
+knlTest = kg.kernel(testPoints, trainingPoints)
 
-        knlRidge = mlpy.KernelRidge( kernel=None)
-        knlRidge.learn(knl, targetValues)
-        resultPoints = knlRidge.pred(knlTest)
+knlRidge = mlpy.KernelRidge( kernel=None)
+knlRidge.learn(knl, targetValues)
+resultPoints = knlRidge.pred(knlTest)
 
-        print(resultPoints)
+print(resultPoints)
 
-        plt.step(trainingPoints, targetValues, 'o')
-        plt.step(testPoints, resultPoints)
-        plt.show()
+plt.step(trainingPoints, targetValues, 'o')
+plt.step(testPoints, resultPoints)
+plt.show()
+
+
 
