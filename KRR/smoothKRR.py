92d75d326a0267187722fd66d35324e5178baf62
smithhan <1139562344@qq.com>
2017-05-26 16:27:36 +0800

    smoothKRR

--- KRR/smoothKRR.py
+++ KRR/smoothKRR.py
@@ -1,11 +1,46 @@
-#coding utf-8
-import numpy as np
-import mlpy
-from pylab import *
-from mlpy import KernelRidge
 import matplotlib.pyplot as plt
+import numpy as np
+from mlpy import KernelGaussian
 import dateutil.parser as dparser
+from pylab import *
+import mlpy
 
-np.random.seed(10)
+def smooth(x,window_len):
 
-tartgetValue=np.genfromtxt("")
+        s=np.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
+        w = np.hamming(window_len)
+
+        y=np.convolve(w/w.sum(),s,mode='same')
+        return y[window_len:-window_len+1]
+
+
+
+        y = np.genfromtxt("/Users/hanzhao/PycharmProjects/MLstudy/file/Gold.csv",
+                     skip_header=1,
+                     dtype=None,
+                     delimiter=',',
+                     usecols=(1))
+
+
+        targetValues = smooth(y, len(y))
+
+
+        np.random.seed(10)
+
+        trainingPoints = np.arange(125).reshape(-1, 1)
+        testPoints = np.arange(126).reshape(-1, 1)
+
+        kg=KernelGaussian()
+        knl = kg.kernel_gaussian(trainingPoints, trainingPoints)
+        knlTest = kg.kernel_gaussian(testPoints, trainingPoints, sigma=1)
+
+        knlRidge = mlpy.KernelRidge( kernel=None)
+        knlRidge.learn(knl, targetValues)
+        resultPoints = knlRidge.pred(knlTest)
+
+        print(resultPoints)
+
+        plt.step(trainingPoints, targetValues, 'o')
+        plt.step(testPoints, resultPoints)
+        plt.show()
+
