5c62999d9b9365ba062a051b4acf08b2cf4aee2c
smithhan <1139562344@qq.com>
2017-06-02 11:27:07 +0800

    PCA

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+"""使用PCA降维算法"""
+
 import matplotlib
 import matplotlib.pyplot as plt
 import mlpy
@@ -6,12 +8,17 @@ import matplotlib.cm as cm
 import numpy as np
 
 wine=np.loadtxt("/Users/hanzhao/PycharmProjects/MLstudy/file/wine.data",delimiter=',')
+
+"""定义特征值"""
 x,y=wine[:,1:4],wine[:,0].astype(np.int)
 print(x.shape)
 print(y.shape)
 
+"""创建新的PCA实例，并根据x轴中选择的特征值来选择相应的算法"""
 pca=mlpy.PCA()
 pca.learn(x)
+
+"""对x轴特征值进行降维，转化为二维子空间"""
 
 z=pca.transform(x,k=2)
 print (z.shape)
