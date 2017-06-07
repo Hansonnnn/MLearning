d32d96bb4dc0ffc2c21a57d2e9f3174fb19b50c0
smithhan <1139562344@qq.com>
2017-06-07 22:12:19 +0800

    SVM

--- SVM/PCA_SVM.py
+++ SVM/PCA_SVM.py
@@ -30,3 +30,24 @@ labx = plt.xlabel("First component")
 laby = plt.ylabel("Second component")
 plt.show()
 
+"""对比SVM"""
+svm=mlpy.LibSvm()
+
+svm.learn(z,y)
+
+xmin,xmax=z[:,0].min()-0.1,z[:,0].max()+0.1
+ymin,ymax=z[:,1].min()-0.1,z[:,1].max()+0.1
+
+xx,yy=np.meshgrid(np.arange(xmin,xmax,0.01),np.arange(ymin,ymax,0.01))
+
+grid = np.c_[xx.ravel(),yy.ravel()]
+
+result=svm.pred(grid)
+fig2=plt.figure(2)
+plot1=plt.pcolormesh(xx,yy,result.reshape(xx.shape),cmap=cm.Greys_r)
+plot2=plt.scatter(z[:,0],z[:,1],c=y,s=90,cmap=cm.Reds)
+labx=plt.xlabel("First component")
+laby=plt.ylabel("Second component")
+limx = plt.xlim(xmin,xmax)
+limy = plt.ylim(ymin,ymax)
+plt.show()
