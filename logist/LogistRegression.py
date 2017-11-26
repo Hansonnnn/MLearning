372a519ebbe37c8d01ad33b03245e6413f877bd5
smithhan <1139562344@qq.com>
2017-11-26 23:08:39 +0800

    batch gradient& soc gradient

--- logist/LogistRegression.py
+++ logist/LogistRegression.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
+from matplotlib import animation
 from logist.load_data import load_data
 import numpy as np
+import random
 
 
 def plot_best_fit(weights):
@@ -23,6 +25,7 @@ def plot_best_fit(weights):
     ax.scatter(x_cord1, y_cord1, s=30, c='red', marker='s')
     ax.scatter(x_cord2, y_cord2, s=30, c='green')
     if weights is not None:
+        x = np.arange(-3.0, 3.0, 0.1)
         y = (-weights[0] - weights[1] * x) / weights[2]
         ax.plot(x, y)
     plt.xlabel('X1');
@@ -30,4 +33,69 @@ def plot_best_fit(weights):
     plt.show()
 
 
-plot_best_fit(None)
+def sigmoid(inx):
+    return 1.0 / (1 + np.exp(-inx))
+
+
+def grad_ascent(data_mat_in, class_labels):
+    """@description the batch grad ascent of logist function
+       @param data_mat_in the input of X matrix
+       @param class_labels the output of Y matrix"""
+
+    data_matrix = np.mat(data_mat_in)
+    label_mat = np.mat(class_labels).transpose()
+    m, n = np.shape(data_matrix)
+    alpha = 0.001  # the learning speed
+    max_cycles = 500
+    weights = np.ones((n, 1))
+    for k in range(max_cycles):
+        h = sigmoid(data_matrix * weights)
+        error = (label_mat - h)
+        weights += alpha * data_matrix.transpose() * error
+    return weights
+
+
+def soc_grad_ascent(data_mat_in, class_label):
+    """@description the stochastic grad ascent of logist function 
+        the main idea of algrithem:
+        step 1 : init every regression's weight with 1
+        step 2 : for every sample:
+                     caculate sample's gradient
+                     alpha * gradient and update weight
+    
+    """
+    data_matrix = np.array(data_mat_in)
+    m, n = np.shape(data_matrix)
+    alpha = 0.01  # the learning speed
+    weights = np.ones(n)
+    for i in range(m):
+        h = sigmoid(data_matrix[i] * weights)
+        error = (class_label[i] - h)
+        weights = weights + alpha * data_matrix[i] * error
+    return weights
+
+
+def soc_grad_ascent1(data_mat_in, class_label, iter_nums=150):
+    """@description the stochastic grad ascent of logist function after modify 
+       @param data_mat_in the input of X matrix
+       @param class_label the output of Y matrix
+       @param iter_nums the bumbers of iterator"""
+    data_matrix = np.array(data_mat_in)
+    m, n = np.shape(data_matrix)
+    weights = np.ones(n)
+    for j in range(iter_nums):
+        data_index = range(m)
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.001
+            rand_index = int(random.uniform(0, len(data_index)))
+            h = sigmoid(sum(data_matrix[rand_index] * weights))
+            error = class_label[rand_index] - h
+            weights = weights + alpha * data_matrix[rand_index] * error
+            np.delete(data_matrix[rand_index])
+    return weights
+
+
+data_arr, label_mat = load_data()
+weights = soc_grad_ascent(data_arr, label_mat)
+
+plot_best_fit(weights)
